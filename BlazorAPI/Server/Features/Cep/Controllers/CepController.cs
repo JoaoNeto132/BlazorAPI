2e0c7313484b67004eeb70b3e0d17cbcbc4926b0
JoaoNeto132 <105755085+JoaoNeto132@users.noreply.github.com>
2022-06-10 11:27:34 -0300

    Adicionar arquivos de projeto.

---
+++ BlazorAPI/Server/Features/Cep/Controllers/CepController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BlazorAPI.Shared.Features.Cep.Models;
+using Newtonsoft.Json;
+
+namespace BlazorAPI.Server.Features.Cep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+        [HttpGet("{cep}")]
+        public Endereco RetornarEndereco(string cep)
+        {
+            var endereco = new Endereco();
+
+            using (var http = new HttpClient())
+            {
+                http.BaseAddress = new Uri("https://viacep.com.br/ws/");
+                
+                try
+                {
+                    var json = http.GetStringAsync($"{cep}/json");
+                    endereco = JsonConvert.DeserializeObject<Endereco>(json.Result);
+                } 
+                    catch
+                {
+                    endereco.erro = true;
+                }
+            }
+
+            return endereco;
+        }
+    }
+}
---
+++ BlazorAPI/Shared/Features/Cep/Models/Endereco.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorAPI.Shared.Features.Cep.Models
+{
+    public class Endereco
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string ddd { get; set; }
+        public bool erro { get; set; }
+
+    }
+}
